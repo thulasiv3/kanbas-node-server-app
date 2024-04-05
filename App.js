a94d53569c3489d1dd15a7ef18f49d4f48add5a1
thulasiv3 <89874040+thulasiv8@users.noreply.github.com>
2024-04-04 21:44:02 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import express from "express";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+import AssignmentRoutes from "./Kanbas/assignments/routes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+CourseRoutes(app);
+Lab5(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
+app.listen(4000);
+app.listen(process.env.PORT || 4000);
